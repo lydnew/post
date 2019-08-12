0bb0c3c95b8c5a9a07b46ba171603c6f22d9b64d
lydnew <liuyandi605@163.com>
2019-08-12 14:20:59 +0800

    添加访问限制中间件

--- post/settings.py
+++ post/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'common.middleware.BlockMiddleware'
 ]
 
 ROOT_URLCONF = 'post.urls'
